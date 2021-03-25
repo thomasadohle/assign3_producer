373bbca9e127464aa8719645070c9dbe354fc723
Thomas Dohle <dohle.t@husky.neu.edu>
2021-03-25 09:35:53 -0400

    added models

---
+++ src/main/java/models/Purchase.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Purchase {
+    private List<PurchaseItem> items;
+    private int storeId;
+    private int customerId;
+    private String date;
+
+    public void setItems(List<PurchaseItem> items){
+        this.items = items;
+    }
+
+    public void setStoreId(int storeId){
+        this.storeId = storeId;
+    }
+
+    public void setCustomerId(int customerId){this.customerId=customerId;}
+
+    public void setDate(String date){this.date=date;}
+
+
+    public List<PurchaseItem> getItems(){return this.items;}
+
+    public int getStoreId(){return this.storeId;}
+
+    public int getCustomerId(){return this.customerId;}
+
+    public String getDate() {return this.date;}
+
+
+
+
+}
